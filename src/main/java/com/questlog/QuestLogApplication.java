f2a1678f72b35b66aa085a073a575c58a3cb02d1
fraidy53 <152137109+fraidy53@users.noreply.github.com>
2025-09-25 14:05:28 +0900

    Update QuestLogApplication.java

--- src/main/java/com/questlog/QuestLogApplication.java
+++ src/main/java/com/questlog/QuestLogApplication.java
@@ -1,13 +1,24 @@
 package com.questlog;
 
+import com.questlog.service.ShopService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QuestLogApplication {
+public class QuestLogApplication implements CommandLineRunner {
+
+    @Autowired
+    private ShopService shopService;
 
     public static void main(String[] args) {
         SpringApplication.run(QuestLogApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        // 애플리케이션 시작 시 상점 데이터 초기화
+        shopService.initializeShopItems();
+    }
 }
